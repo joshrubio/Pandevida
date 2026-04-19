f13c01ba8935d977d6a5a16f29a8c7ce9417a1db
joshrubio <155823600+joshrubio@users.noreply.github.com>
2026-04-19 14:15:26 +0200

    Feat: add G4A integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,18 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WMLHR878L6"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WMLHR878L6');
+        `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
